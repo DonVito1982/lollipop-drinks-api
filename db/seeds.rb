9c09982074898c9e10f67d253b6a17e55dc94673
Víctor Rafael Zambrano Sarabia <donvito4571@gmail.com>
2025-04-15 17:09:40 -0400

    Seed drink information

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+initial_drinks = [
+  {name: "Monster Ultra Sunrise", serv_count: 2, serv_caffeine: 75},
+  {name: "Black Coffee", serv_count: 1, serv_caffeine: 95},
+  {name: "Americano", serv_count: 1, serv_caffeine: 77},
+  {name: "Sugar free NOS ", serv_count: 2, serv_caffeine: 130},
+  {name: "5 Hour Energy", serv_count: 1, serv_caffeine: 200},
+]
+
+initial_drinks.each do |drink_attrs|
+  Drink.find_or_create_by!(**drink_attrs)
+end
