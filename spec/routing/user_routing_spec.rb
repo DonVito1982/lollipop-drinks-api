84cb729ad91dffa9633a862b0f25aa5a0bcf3b58
Víctor Rafael Zambrano Sarabia <donvito4571@gmail.com>
2025-04-17 07:43:30 -0400

    Check user routing

---
+++ spec/routing/user_routing_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe UserDrinksController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/caffeine-count").to route_to("users#caffeine_count")
+    end
+
+    it "routes to #index" do
+      expect(get: "/me").to route_to("users#me")
+    end
+
+    it "routes to #create" do
+      expect(post: "/users").to route_to("users#create")
+    end
+  end
+end
